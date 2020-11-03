21588cf335a02aa51ccfae32189544abb57a2ebb
Labenea <edokaditya@gmail.com>
2020-11-03 22:54:56 +0800

    adding uts homepage

---
+++ UTS/webpack.common.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/pages/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
